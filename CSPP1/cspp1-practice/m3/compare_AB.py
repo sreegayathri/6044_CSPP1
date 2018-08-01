6603225e37896b93226e243fac31539ef32d0e0f
sreegayathri <sree0291@msitprogram.net>
2018-08-01 21:07:06 +0530

    5 m3

---
+++ CSPP1/cspp1-practice/m3/compare_AB.py
@@ -0,0 +1,11 @@
+varA=10
+varB="happy"
+if varA=="str" or varB=="str":
+	print("string involved")
+elif varA>varB:
+		print("bigger")
+elif varA==varB:
+			print("equal")
+else varA<varB:
+				print("smaller")
+		
