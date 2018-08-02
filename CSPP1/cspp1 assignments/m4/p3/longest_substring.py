7184cba40f28e75da81fa1853486c2381508a0bf
sreegayathri <sree0291@msitprogram.net>
2018-08-02 20:38:57 +0530

    " -> 0 of 5 passed. pylint: 6.15/10 "

--- CSPP1/cspp1 assignments/m4/p3/longest_substring.py
+++ CSPP1/cspp1 assignments/m4/p3/longest_substring.py
@@ -18,19 +18,19 @@ If you have time, come back to this problem after you've had a break and cleared
 
 def main():
 	""" Longest substring in alphabetical order """
-	string = "abcdbc"
-	s = 0
-	count = 0
-	count1 = 0
-	for i in range(len(string) - 1):# comparing the indices
-    	if string[i] <= string[i+1]:# comparing the characters in the indices
-        	count += 1
-        	if count > count1:
-            	count1 = count
-            	s = i + 1
+	string_ab = "abcdbc"
+	s_ab = 0
+	count_ab = 0
+	count1_ab = 0
+	for i in range(len(string_ab) - 1):# comparing the indices
+    	if string_ab[i] <= string_ab[i+1]:# comparing the characters in the indices
+        	count_ab += 1
+        	if count_ab > count1_ab:
+            	count1_ab = count_ab
+            	s_ab = i + 1
     	else:
-        count = 0
-	str1 = s - count1
-print(string[str1:s+1])
+        count_ab = 0
+	str1_ab = s_ab - count1_ab
+print(string_ab[str1_ab:s_abc+1])
 if __name__ == "__main__":
 	main()
