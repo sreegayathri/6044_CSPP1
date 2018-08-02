fb89887cd95d555fabd52d9c040e706d2632a636
sreegayathri <sree0291@msitprogram.net>
2018-08-02 19:21:44 +0530

    " -> 0 of 1 passed. pylint: 8.00/10 "

--- CSPP1/sample-assignment/sol.py
+++ CSPP1/sample-assignment/sol.py
@@ -1,8 +1,8 @@
-# Exercise: Assignment-1
+""" Exercise: Assignment-1
 # Write a Python function, factorial(n), that takes in one number and
 # returns the factorial of given number.
 
-# This function takes in one number and returns one number.
+# This function takes in one number and returns one number."""
 
 def main():
     data = input()
