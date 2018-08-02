b8f886d686afdebfce7c5c6978a69e39d22b0af1
sreegayathri <sree0291@msitprogram.net>
2018-08-02 21:02:32 +0530

    " -> 0 of 5 passed. pylint: 0.00/10 "

--- CSPP1/cspp1 assignments/m4/p3/longest_substring.py
+++ CSPP1/cspp1 assignments/m4/p3/longest_substring.py
@@ -19,19 +19,19 @@ cleared your head.'''
 
 def main():
     """ Longest substring in alphabetical order """
-    STRING_AB = "abcdbc"
-    S_AB = 0
-    COUNT_AB = 0
-    COUNT1_AB = 0
-    for i in range(len(STRING_AB) - 1):# comparing the indices
-        if STRING_AB[i] <= STRING_AB[i+1]:# comparing the characters in the indices
-            COUNT_AB += 1
-            if COUNT_AB > COUNT1_AB:
-                COUNT1_AB = COUNT_AB
-                S_AB = i + 1
+    string_ab = "abcdbc"
+    s_ab = 0
+    count_ab = 0
+    count1_ab = 0
+    for i in range(len(string_ab) - 1):# comparing the indices
+        if string_ab[i] <= string_ab[i+1]:# comparing the characters in the indices
+            count_ab += 1
+            if count_ab > count1_ab:
+                count1_ab = count_ab
+                s_ab = i + 1
         else:
-            COUNT_AB = 0
-    STR_AB = S_AB - COUNT1_AB
-print(STRING_AB[STR_AB:S_AB + 1])
+            count_ab = 0
+    str_ab = s_ab - count1_ab
+print(string_ab[str_ab:s_ab + 1])
 if __name__ == "__main__":
     main()
