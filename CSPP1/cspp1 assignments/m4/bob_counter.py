b3b64009ba182e52524bbaf67e656b0d4d796815
sreegayathri <sree0291@msitprogram.net>
2018-08-02 20:17:11 +0530

    " -> 5 of 5 passed. pylint: 4.17/10 "

--- CSPP1/cspp1 assignments/m4/bob_counter.py
+++ CSPP1/cspp1 assignments/m4/bob_counter.py
@@ -7,14 +7,14 @@ Number of times bob occurs is: 2'''
 
 def main():
     """ to count the number of times a substring occurs"""
-S = 'azcbobobobegghakl'
-SUB = 'bob'
-RESULTS = 0
-S_LEN = len(S)
-SUB_LEN = len(SUB)
-for i in range(len(S)):
-    if S[i:i+SUB_LEN] == SUB:
-        RESULTS += 1
-print(RESULTS)
+    S = input()
+    SUB = 'bob'
+    RESULTS = 0
+    S_LEN = len(S)
+    SUB_LEN = len(SUB)
+    for i in range(len(S)):
+        if S[i:i+SUB_LEN] == SUB:
+            RESULTS += 1
+    print(RESULTS)
 if __name__ == "__main__":
     main()
