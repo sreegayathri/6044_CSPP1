0c7a0014ae912cae0f71c60a0987561cc9768455
sreegayathri <sree0291@msitprogram.net>
2018-08-04 20:32:29 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

---
+++ CSPP1/cspp1-assignments/m5/p1/perfect_cube.py
@@ -0,0 +1,28 @@
+# Write a python program to find if the given number is a perfect cube or not
+# using guess and check algorithm
+
+# testcase 1
+# Input: 24389
+# Output: 24389 is a perfect cube
+
+# testcase 2
+# Input: 21950
+# Output: 21950 is not a perfect cube
+
+def main():
+    num_va = int(input())
+guess = 0
+epsilon = 0.1
+inc = 0.1
+num_a = 0
+    while guess < num_va:
+        if abs(guess**3-num_va) < epsilon:
+            break
+        guess = guess+inc
+        num_a += 1
+    if abs(guess**3-num_va) >= epsilon:
+        print(num_va, "is not a perfect cube")
+    else:
+        print(num_va, "is a perfect cube")
+if __name__== "__main__":
+    main()
--- CSPP1/cspp1-assignments/m5/perfect_cube.py
+++ CSPP1/cspp1-assignments/m5/perfect_cube.py
@@ -1,11 +1,14 @@
-input_n = int(input())
-result_a = 0
-for i in range(1,input_n+1,1):
-	if i*i*i == input_n:
-		result_a = 1
-	else:
-		result_a = 0
-if result_a == 1:
-	print("perfect cube")
+num_va = int(input())
+guess = 0
+epsilon = 0.1
+inc = 0.1
+num_a = 0
+while guess < num_va:
+    if abs(guess**3-num_va) < epsilon:
+        break
+    guess = guess+inc
+    num_a += 1
+if abs(guess**3-num_va) >= epsilon:
+    print(num_va, "is not a perfect cube")
 else:
-	print("the number is not a perfect cube")
+    print(num_va, "is a perfect cube")
