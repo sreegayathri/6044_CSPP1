216f8d658be60f9da716551741149ff489132a7d
sreegayathri <sree0291@msitprogram.net>
2018-08-04 21:24:15 +0530

    " -> 3 of 3 passed. pylint: 8.89/10 "

--- CSPP1/cspp1-assignments/m5/p2/square_root.py
+++ CSPP1/cspp1-assignments/m5/p2/square_root.py
@@ -1,12 +1,13 @@
 """Write a python program to find the square root of the given number
 using approximation method
-testcase 1
+"""
+def main():
+    """testcase 1
 input: 25
 output: 4.999999999999998
 testcase 2
 input: 49
 output: 6.999999999999991"""
-def main():
     num_val = int(input())
     epsilon = 0.1
     guess = 0
