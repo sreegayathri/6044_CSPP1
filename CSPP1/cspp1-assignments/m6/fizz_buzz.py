cd37c92dccbded5b52eb2309537550d8222138ca
sreegayathri <sree0291@msitprogram.net>
2018-08-04 11:17:19 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- CSPP1/cspp1-assignments/m6/fizz_buzz.py
+++ CSPP1/cspp1-assignments/m6/fizz_buzz.py
@@ -1,11 +1,11 @@
 num_a = int(input())
 if num_a > 0:
-	if num_a % 3 == 0:
-		print("Fizz")
-	elif num_a % 5 == 0:
-		print("Buzz")
-else:
-	print("not a multiple of 3 and 5")
+    if num_a % 3 == 0:
+        print("Fizz")
+    elif num_a % 5 == 0:
+        print("Buzz")
 if num_a % 3 == 0 and num_a % 5 ==0:
-		print("Fizz")
-		print("Buzz")
+        print("Fizz")
+        print("Buzz")
+else:
+    print("not a multiple of 3 and 5")
