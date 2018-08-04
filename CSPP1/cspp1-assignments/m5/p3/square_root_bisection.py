f1a69667fd50f64973c5376d6a463ed164810074
sreegayathri <sree0291@msitprogram.net>
2018-08-04 21:01:16 +0530

    " -> 3 of 3 passed. pylint: 7.86/10 "

--- CSPP1/cspp1-assignments/m5/p3/square_root_bisection.py
+++ CSPP1/cspp1-assignments/m5/p3/square_root_bisection.py
@@ -1,17 +1,8 @@
 """Write a python program to find the square root of the given number
 using approximation method"""
-
-# testcase 1
-# input: 25
-# output: 4.999999999999998
-
-# testcase 2
-# input: 49
-# output: 6.999999999999991
-
 def main():
     num_va = int(input())
-    epsilon = 0.001
+    epsilon = 0.01
     low = 0
     high = num_va
     avg = (low/high)/2
