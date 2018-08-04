a8d044ecfd056eeb8d2e64c323349c0c1fb79925
sreegayathri <sree0291@msitprogram.net>
2018-08-04 12:50:22 +0530

    " -> 0 of 7 passed. pylint: 9.09/10 "

--- CSPP1/cspp1-assignments/m6/digit_product.py
+++ CSPP1/cspp1-assignments/m6/digit_product.py
@@ -1,9 +1,9 @@
-n_a =int(input())
+n_a = int(input())
 product = 1
 i = 0
 r_a = 0
 if n_a > 0:
-	if n_a % 10!=0:
-		r_a = n_a(i)
-		product *= r_a
+    if n_a % 10 != 0:
+        r_a += 1
+        product *= r_a
 print(product)
--- CSPP1/cspp1-assignments/m6/p3/digit_product.py
+++ CSPP1/cspp1-assignments/m6/p3/digit_product.py
@@ -10,7 +10,6 @@ def main():
     '''
     n_a = int(input())
     product = 1
-    i = 0
     r_a = 0
     if n_a > 0:
         if n_a % 10 != 0:
