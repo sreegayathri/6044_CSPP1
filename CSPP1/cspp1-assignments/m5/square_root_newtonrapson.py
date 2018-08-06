ea81e1ae180b7260d76bb7ace2a9c4dca91a1e29
sreegayathri <sree0291@msitprogram.net>
2018-08-06 11:24:02 +0530

    square

--- CSPP1/cspp1-assignments/m5/square_root_newtonrapson.py
+++ CSPP1/cspp1-assignments/m5/square_root_newtonrapson.py
@@ -2,5 +2,5 @@ num_val = int(input())
 guess = num_val/2.0
 epsilon = 0.01
 while (guess*guess-num_val) >= epsilon and guess <= num_val:
-	guess = guess-(((guess**2)-num_val)/(2*guess))
+    guess = guess-(((guess**2)-num_val)/(2*guess))
 print(guess)
