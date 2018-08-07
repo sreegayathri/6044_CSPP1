21bced29512626b47c4d2050c16de8f30013e411
sreegayathri <sree0291@msitprogram.net>
2018-08-07 19:52:56 +0530

    " -> 10 of 10 passed. pylint: 5.38/10 "

--- CSPP1/cspp1-assignments/m8/p2/assignment2.py
+++ CSPP1/cspp1-assignments/m8/p2/assignment2.py
@@ -1,8 +1,9 @@
-# Exercise: Assignment-2
-# Write a Python function, sumofdigits, that takes in one number and returns the sum of digits of given number.
+""" Exercise: Assignment-2
+# Write a Python function, sumofdigits, that takes in one 
+number and returns the sum of digits of given number.
 
 # This function takes in one number and returns one number.
-
+"""
 
 def sumofdigits(n):
     '''
