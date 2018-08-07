b184c9a8b19ce31dbc65b1f1a1583c23c607ce7d
sreegayathri <sree0291@msitprogram.net>
2018-08-07 18:15:44 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- CSPP1/cspp1-practice/m8/GCD using Iteration/gcd_iter.py
+++ CSPP1/cspp1-practice/m8/GCD using Iteration/gcd_iter.py
@@ -17,7 +17,6 @@ def gcdIter(a, b):
     for i in range(1, smaller+1):
         if((a % i == 0) and (b % i == 0)):
             gcdIter = i
-            
     return gcdIter
 def main():
     data = input()
