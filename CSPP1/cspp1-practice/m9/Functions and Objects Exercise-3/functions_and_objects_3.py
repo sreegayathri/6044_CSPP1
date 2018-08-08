8b5b17b2985012b5b65255159abcc9165186f41f
sreegayathri <sree0291@msitprogram.net>
2018-08-08 20:55:43 +0530

    square

--- CSPP1/cspp1-practice/m9/Functions and Objects Exercise-3/functions_and_objects_3.py
+++ CSPP1/cspp1-practice/m9/Functions and Objects Exercise-3/functions_and_objects_3.py
@@ -1,19 +1,20 @@
-#Exercise : Function and Objects Exercise-3
-#Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 16, 64, 81]
+"""#Exercise : Function and Objects Exercise-3
+#Implement a function that converts the given testList = [1, -4, 8, -9]
+ into [1, 16, 64, 81]"""
 
 
-def apply_to_each(L, f):
-    """:param L the list of elements
-    #:param f is the function that takes L elements to give the required o/p
+def apply_to_each(l_a, f_a):
+    """:param l_a the list of elements
+    #:param f_a is the function that takes L elements to give the required o/p
     #:return int type"""
-    for i in range(len(L)):
-        L[i] = f(L[i])
-    return L
-def square(L):
+    for i in enumerate(l_a):
+        l_a[i] = f_a(l_a[i])
+    return l_a
+def square(l_a):
     """takes the return value from apply_to_each"""
-    return L*L
+    return l_a*l_a
 def main():
-    """using the condition from above and input here 
+    """using the condition from above and input here
     print"""
     data = input()
     data = data.split()
@@ -22,5 +23,5 @@ def main():
         list1.append(int(j))
     print(apply_to_each(list1, square))
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
