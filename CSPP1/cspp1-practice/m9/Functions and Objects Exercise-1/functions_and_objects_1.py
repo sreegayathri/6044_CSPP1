63cda84f4e3e56a41a32f6e631f21f9d259ac40e
sreegayathri <sree0291@msitprogram.net>
2018-08-08 20:41:13 +0530

    abs

--- CSPP1/cspp1-practice/m9/Functions and Objects Exercise-1/functions_and_objects_1.py
+++ CSPP1/cspp1-practice/m9/Functions and Objects Exercise-1/functions_and_objects_1.py
@@ -3,13 +3,18 @@
 testList = [1, -4, 8, -9] into [1, 4, 8, 9]
 """
 
-def apply_to_each(L, f):
-
-    for i in range(len(L)):
-        L[i] = f(L[i])
-    return L
+def apply_to_each(l_a, f_a):
+    """if th input is [1, -8, 2], then output is [1, 8, 2]
+    :param filename: L - array of int/char elements
+    :param filename: f - function type used on the list elements
+    :return: int"""
+    for i in enumerate(l_a):
+        l_a[i] = f_a(l_a[i])
+    return l_a
 
 def main():
+    """using the condition from above and input here
+    print """
     data = input()
     data = data.split()
     list1 = []
