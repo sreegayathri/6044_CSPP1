7eb821a8d31b77603c5a3ed7b67d7d4ed5ae5cda
sreegayathri <sree0291@msitprogram.net>
2018-08-11 12:39:36 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

--- CSPP1/cspp1-assignments/m12/p1/create_social_network.py
+++ CSPP1/cspp1-assignments/m12/p1/create_social_network.py
@@ -4,41 +4,39 @@
 
 def create_social_network(data_):
     '''
-        The data argument passed to the function is a string
+        The data argument passed to the function is a data_
         It represents simple social network data
         In this social network data there are people following other people
 
-        Here is an example social network data string:
+        Here is an example social network data data_:
         John follows Bryant,Debra,Walter
         Bryant follows Olive,Ollie,Freda,Mercedes
         Mercedes follows Walter,Robin,Bryant
         Olive follows John,Ollie
 
-        The string has multiple lines and each line represents one person
+        The data_ has multiple lines and each line represents one person
         The first word of each line is the name of the person
         The second word is follows that separates the person from the followers
         After the second word is a list of people separated by ,
 
-        create_social_network function should split the string on lines
+        create_social_network function should split the data_ on lines
         then extract the person and the followers by splitting each line
         finally add the person and the followers to a data_onary and
         return the data_onary
 
-        Caution: watch out for trailing spaces while splitting the string.
+        Caution: watch out for trailing spaces while splitting the data_.
         It may cause your test cases to fail although your output may be right
 
         Error handling case:
-        Return a empty data_onary if the string format of the data is invalid
+        Return a empty data_onary if the data_ format of the data is invalid
         Empty data_onary is not None, it is a data_onary with no keys
     '''
-
-    b_file = open('input001.txt', 'r')
+    b_a = input()
     data_ = {}
-    for i in open('input001.txt', 'r'):
-        keys, values = i.split()
-        if keys in data_.keys():
-            if values not in data_[keys]:
-                data_[keys].append(values)
+    keys, values = b_a.split()
+    if keys in data_.keys():
+        if values not in data_[keys]:
+            data_[keys].append(values)
         else:
             data_[keys] = [values]
     return data_
@@ -47,14 +45,14 @@ def main():
     '''
         handling testcase input and printing output
     '''
-    string = ''
+    data_ = ''
     lines = int(input())
     for i in range(lines):
         i += 1
-        string += input()
-        string += '\n'
+        data_ += input()
+        data_ += '\n'
 
-    print(create_social_network(string))
+    print(create_social_network(data_))
 
 if __name__ == "__main__":
     main()
