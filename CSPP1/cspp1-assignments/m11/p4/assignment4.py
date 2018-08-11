a9b6644c5d29333812ccc865b05dfba2495d70f5
sreegayathri <sree0291@msitprogram.net>
2018-08-11 22:44:07 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

--- CSPP1/cspp1-assignments/m11/p4/assignment4.py
+++ CSPP1/cspp1-assignments/m11/p4/assignment4.py
@@ -1,33 +1,30 @@
 #Exercise: Assignment-4
-#We are now ready to begin writing the code that interacts with the player. We'll be implementing the playHand function. This function allows the user to play out a single hand. First, though, you'll need to implement the helper calculateHandlen function, which can be done in under five lines of code.
+#We are now ready to begin writing the code that interacts with the player. We'll be implementing the playHand function. This function allows the user to play out a single hand. First, though, you'll need to implement the helper calculate_Hand_len function, which can be done in under five lines of code.
 
 
-def calculateHandlen(hand):
+def calculate_hand_len(hand):
     """ 
-    Returns the length (number of letters) in the current hand.
-    
-    hand: dictionary (string int)
-    returns: integer
+    :param hand: dictionary (string int)
+    :returns: integer type
     """
-    temp = ""
+    temp_a = ""
     length_of_hand = 0
     for letter in hand.keys():
         for count in range(hand[letter]):
-            temp = temp + letter
-    length_of_hand = len(temp)
+            temp_a = temp_a + letter
+    length_of_hand = len(temp_a)
     return length_of_hand 
    
 
 def main():
-    n=input()
-    adict={}
-    for i in range(int(n)):
-        data=input()
-        l=data.split()
-        adict[l[0]]=int(l[1])
-    print(calculateHandlen(adict))
-        
+    """getting the required condition by calling calculate_hand_len into main"""
+    n_input = input()
+    adict = {}
+    for i in range(int(n_input)):
+        data = input()
+        list_a = data.split()
+        adict[list_a[0]] = int(list_a[1])
+    print(calculate_hand_len(adict)
 
-
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
