eca21854ae587d490f9539fec2f013536cea8c45
sreegayathri <sree0291@msitprogram.net>
2018-08-11 22:53:10 +0530

    " -> 0 of 5 passed. pylint: 1.67/10 "

--- CSPP1/cspp1-assignments/m11/p4/assignment4.py
+++ CSPP1/cspp1-assignments/m11/p4/assignment4.py
@@ -1,6 +1,10 @@
-#Exercise: Assignment-4
-#We are now ready to begin writing the code that interacts with the player. We'll be implementing the playHand function. This function allows the user to play out a single hand. First, though, you'll need to implement the helper calculateHandlen function, which can be done in under five lines of code.
-
+"""#Exercise: Assignment-4
+#We are now ready to begin writing the code that interact
+with the player. We'll be implementing the playHand function.
+This function allows the user to play out a single hand.
+First, though, you'll need to implement the helper
+calculateHandlen function, which can be done in under five lines of code.
+"""
 
 def calculateHandlen(hand):
     """ 
