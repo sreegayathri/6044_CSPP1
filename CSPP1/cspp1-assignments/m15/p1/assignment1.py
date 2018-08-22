a6f5bdd3753c88065f1d539ee203125b17cf8a7b
sreegayathri <sree0291@msitprogram.net>
2018-08-22 20:45:26 +0530

    " -> 3 of 3 passed. pylint: 9.65/10 "

--- CSPP1/cspp1-assignments/m15/p1/assignment1.py
+++ CSPP1/cspp1-assignments/m15/p1/assignment1.py
@@ -58,7 +58,7 @@ def get_story_string():
 
 WORDLIST_FILENAME = 'words.txt'
 
-class Message:
+class Message(object):
     ''' Message object '''
     ### DO NOT MODIFY THIS METHOD ###
     def __init__(self, text):
@@ -73,7 +73,6 @@ class Message:
         '''
         self.message_text = text
         self.valid_words = load_words("words.txt")
-        self.build_shift_dict = {}
 
     ### DO NOT MODIFY THIS METHOD ###
     def get_message_text(self):
@@ -230,7 +229,6 @@ class CiphertextMessage(Message):
         '''
         self.message_text = text
         self.valid_words = load_words("words.txt")[:]
-        Message.__init__(self, text)
         self.max_valid_words = 0
         self.decrypted_message = ()
 
