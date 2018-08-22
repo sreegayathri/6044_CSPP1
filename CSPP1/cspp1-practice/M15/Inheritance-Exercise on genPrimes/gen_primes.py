6d5d989360b20633ca54bb1e766b3bb2b213b109
sreegayathri <sree0291@msitprogram.net>
2018-08-23 02:42:05 +0530

    " -> 7 of 7 passed. pylint: 8.57/10 "

--- CSPP1/cspp1-practice/M15/Inheritance-Exercise on genPrimes/gen_primes.py
+++ CSPP1/cspp1-practice/M15/Inheritance-Exercise on genPrimes/gen_primes.py
@@ -1,17 +1,28 @@
 #define the gen_primes function here
-def genPrimes():
-    pass
+def prime_num(a):
+    for number in range(2, a):
+        if a % number == 0:
+            return False
+    return True
+
+def gen_primes():
+    digit = 2
+    while True:
+        if prime_num(digit):
+            yield digit
+        digit += 1
+
 
 def main():
-	data=raw_input()
-	l=data.split()
-	a=int(l[0])
-	b=int(l[1])
-	primeGenerator = genPrimes()
-	for i in range(a):
-	    p=primeGenerator.next()
-	    if(i%b==0):
-	        print p
-	
+    data = input()
+    l = data.split()
+    a = int(l[0])
+    b = int(l[1])
+    primeGenerator = gen_primes()
+    for i in range(a):
+        p = primeGenerator.__next__()
+        if(i%b == 0):
+            print(p)
+    
 if __name__== "__main__":
-	main()
+    main()
