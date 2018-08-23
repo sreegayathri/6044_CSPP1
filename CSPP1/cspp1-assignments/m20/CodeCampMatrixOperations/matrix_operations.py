8af841a181b0d2af7a11f40c0134b6bdf3c5cd58
sreegayathri <sree0291@msitprogram.net>
2018-08-23 21:13:03 +0530

    " -> 0 of 8 passed. pylint: 5.68/10 "

--- CSPP1/cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ CSPP1/cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -1,14 +1,25 @@
 def mult_matrix(m1, m2):
     '''
-        check if the matrix1 columns = matrix2 rows
+        check if the matx1 columns = matx2 rows
         mult the matrices and return the result matrix
         print an error message if the matrix shapes are not valid for mult
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    if matrix1[i][j] == matrix2[i][j]:
-        result_matrix1 = matrix1[i][j] * matrix2[j][i] 
-    return result_matrix
+    if len(m1[0]) != len(m2) and len(m1) != len(m2[0]):
+        print("Error: Matrix shapes invalid for mult")
+        return None
+    multiply = []
+    for i in range(len(m1)):
+        temp_mul = []
+        for j in range(len(m2[0])):
+            res = 0
+            for k in range(m1[0]):
+                res += [m1[i][k]*m2[k][j]]
+        multiply.append(temp_mul)
+    return multiply
+
+
 def add_matrix(m1, m2):
     '''
         check if the matrix shapes are similar
@@ -17,11 +28,21 @@ def add_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for addition"
     '''
-    if matrix1[i][j] == matrix2[i][j]:
-        result_matrix2 = matrix1[i][j] + matrix2[i][j] 
-    return result_matrix
-    
+    if len(m1) != len(m2) & len(m1[0]) != len(m2[0]):
+        print("Error: Matrix shapes invalid for addition")
+        return None
+    add1 = []
+    for i in range(len(m1)):
+        temp_sum = []
+        for j in range(len(m1[0])):
+            sum_matrix = m1[i][j] + m2[i][j]
+            temp_sum.append(sum_matrix)
+        add1.append(temp_sum)
+    print(add1)
 
+    #return add_matrix
+
+            
 def read_matrix():
     '''
         read the matrix dimensions from input
@@ -30,19 +51,37 @@ def read_matrix():
         print an error message and return None
         error message should be "Error: Invalid input for the matrix"
     '''
-    row_matrix = int(input("Enter a number: "))
-    column _matrix = int(input("Enter a number: ")) 
+    dim = input().split(',')
+    matrix = []
+    for i in range(0, int(dim[0]), 1):
+        values = input()
+        temp = values.split(" ")
+        k = []
+        for j in temp:
+            k.append(int(j))
+        matrix.append(k)
+    print(matrix)
+    # print(m1)
+    # dim2 =input().split(',')
+    # for i in range(0, int(dim2[0]),1):
+    #     elements = input()
+    #     temp2 = elements.split(" ")
+    #     m = []
+    #     for j in temp2:
+    #         m.append(int(j))
+    #     m2.append(m)
+    # print(m2)
+    return matrix   
 
 def main():
     # read matrix 1
-    matrix1 = read_matrix()
+    mx1 = read_matrix()
     # read matrix 2
-    matrix2 = read_matrix()
-
+    mx2 = read_matrix()
     # add matrix 1 and matrix 2
-    add_matrix = add_matrix(matrix1, matrix2)
+    addtion_x = add_matrix(mx1, mx2)
     # multiply matrix 1 and matrix 2
-    multiply_matrix = mult_matrix(matrix1, matrix2)
+    multiply_matrix = mult_matrix(mx1, mx2)
 
 if __name__ == '__main__':
     main()
