85d83ee1229269a3ca6c8ddb2caeed3ea7497da8
sreegayathri <sree0291@msitprogram.net>
2018-08-24 17:35:57 +0530

    " -> 14 of 14 passed. pylint: 8.24/10 "

--- CSPP1/cspp1-assignments/m21-codecamp/tictocto.py
+++ CSPP1/cspp1-assignments/m21-codecamp/tictocto.py
@@ -1,39 +1,27 @@
 def row_check(matrix):
-
+    """to check if row has similar values x/o/."""
     if matrix[0][0] == matrix[0][1] == matrix[0][2]:
         return matrix[0][0]
     if matrix[1][0] == matrix[1][1] == matrix[1][2]:
         return matrix[1][1]
     if matrix[2][0] == matrix[2][1] == matrix[2][2]:
         return matrix[2][0]
-
 def col_check(matrix):
-
+    """to check if column has similar values x/o/."""
     if matrix[0][0] == matrix[1][0] == matrix[2][0]:
         return matrix[0][0]
     if matrix[0][1] == matrix[1][1] == matrix[2][1]:
         return matrix[1][1]
     if matrix[0][2] == matrix[1][2] == matrix[2][2]:
         return matrix[0][2]
-
 def diagonal_check(matrix):
-
+    """to check if diagonal has similar values x/o/."""
     if matrix[0][0] == matrix[1][1] == matrix[2][2]:
         return matrix[0][0]
     if matrix[0][2] == matrix[1][1] == matrix[2][0]:
         return matrix[1][1]
-# def read_data():
-
-#     matrix = []
-#     for _ in range(0, 3, 1):
-#         values = input()
-#         temp = values.split(" ")
-#         k = []
-#         for j in temp:
-#             k.append(j)
-#         matrix.append(k)
-#     return matrix
 def matrix_check(matrix):
+    """to check if input has values x/o/."""
     for element in matrix:
         for j in element:
             if j == 'x' or j == 'o' or j == '.':
@@ -42,6 +30,7 @@ def matrix_check(matrix):
                 return False
     return True
 def matrix_game(matrix):
+    """to check if the game is valid or not with x/o elements with differnceof 1/2"""
     element_x = 0
     element_o = 0
     element_dot = 0
@@ -58,10 +47,8 @@ def matrix_game(matrix):
     elif abs(element_x - element_o == 0) and element_dot > 0:
         return False
     return True 
-
 def main():
     """ main function of the matrix-operation"""
-    # read matrix
     win = None
     matrix = []
     for i in range(0, 3, 1):
@@ -70,7 +57,6 @@ def main():
         for j in values:
             temp.append(j)
         matrix.append(temp)
-    #print("matrix: ",matrix)
     tictokto_matrix = matrix_check(matrix) and matrix_game(matrix)
     if tictokto_matrix == True:
         win = row_check(matrix)
